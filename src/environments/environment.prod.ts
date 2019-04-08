ddee891fdbc5105af7e391bec0264a3a7f784c04
Dave Rojas <davejrojas@gmail.com>
2019-04-08 09:16:41 +0200

    logos update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 const packageJson = require('../../package.json');
 
 export const environment = {
-  appName: 'Angular Ngrx Material Starter',
+  appName: 'Troovix',
   envName: 'PROD',
   production: true,
   test: false,
